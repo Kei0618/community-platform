938fd9e771cbb2cbf7d867715f0d78bb8e654fe3
LIYAOLI-TM56NHA\Administrator <adsf8564@gmail.com>
2023-08-21 23:45:07 +0800

    add elementplus

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,23 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+//ElementPlus套件
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
+  // 實際路徑轉換 @->src
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
