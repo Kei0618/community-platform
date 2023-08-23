1d79227c69a12ced8ef78f9bcd5cdb2b3664ccbd
LIYAOLI-TM56NHA\Administrator <adsf8564@gmail.com>
2023-08-23 17:41:21 +0800

    page  revised

--- src/views/Login/application.vue
+++ src/views/Login/application.vue
@@ -1,46 +1,78 @@
 <template>
-    <div>
+  <div class="container-md mt-5">
+    <div class="application-form">
       <h2>帳號申請</h2>
       <form @submit.prevent="submitForm">
-        <div class="mb-3">
+        <div class="form-group ">
           <label for="username" class="form-label">使用者名稱</label>
           <input type="text" id="username" v-model="username" class="form-control" required>
         </div>
-        <div class="mb-3">
-          <label for="email" class="form-label">電子郵件</label>
-          <input type="email" id="email" v-model="email" class="form-control" required>
+        <div class="form-group ">
+          <label for="account" class="form-label">帳號</label>
+          <input type="password" id="account" v-model="password" class="form-control" required>
         </div>
-        <div class="mb-3">
+        <div class="form-group ">
           <label for="password" class="form-label">密碼</label>
           <input type="password" id="password" v-model="password" class="form-control" required>
         </div>
-        <button type="submit" class="btn btn-primary">申請帳號</button>
+        <div class="form-group ">
+          <label for="email" class="form-label">電子郵件</label>
+          <input type="email" id="email" v-model="email" class="form-control" required>
+        </div>
+        <div class="form-group ">
+          <button type="submit" class="btn btn-primary ">申請帳號</button>
+          <router-link class="btn btn-secondary mx-3" to="/">返回首頁</router-link>
+        </div>
       </form>
     </div>
-  </template>
+  </div>
+</template>
   
-  <script>
-  export default {
-    data() {
-      return {
-        username: '',
-        email: '',
-        password: '',
-      };
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    submitForm() {
+      // 在這裡可以處理帳號申請的邏輯，例如發送 API 請求
+      console.log('使用者名稱:', this.username);
+      console.log('電子郵件:', this.email);
+      console.log('密碼:', this.password);
+
+      // 進行 API 請求等處理後，可以導航到其他頁面
+      // this.$router.push('/login'); // 導航到登入頁面
     },
-    methods: {
-      submitForm() {
-        // 在這裡可以處理帳號申請的邏輯，例如發送 API 請求
-        console.log('使用者名稱:', this.username);
-        console.log('電子郵件:', this.email);
-        console.log('密碼:', this.password);
-        
-        // 進行 API 請求等處理後，可以導航到其他頁面
-        // this.$router.push('/login'); // 導航到登入頁面
-      },
-    },
-  };
-  </script>
+  },
+};
+</script>
+
+<style scoped>
+.application-form {
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 60px;
+  text-align: center;
+  border: 1px solid #ccc;
+  border-radius: 20px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.form-group {
+  margin-bottom: 20px;
+}
+
+.form-control {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+</style>
   設定 Vue Router：
   在你的 Vue Router 設定中，將 /signup 路由指向剛剛建立的 Signup.vue 組件。
   
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -2,105 +2,108 @@
 import LayoutHeader from '@/views/Layout/component/LayoutHeader.vue';
 </script>
 <template>
-    
+  <LayoutHeader />
+  <div class="container-md mt-5">
     <div class="login-page">
       <div class="login-form">
-      <h1>登入</h1>
-      <form @submit.prevent="login">
-        <div class="form-group">
-          <label for="username">用戶名</label>
-          <input v-model="username" type="text" id="username" class="form-control">
+        <h1>登入</h1>
+        <form @submit.prevent="login">
+          <div class="form-group">
+            <label for="username">帳號</label>
+            <input v-model="username" type="text" id="username" class="form-control">
+          </div>
+          <div class="form-group">
+            <label for="password">密碼</label>
+            <input v-model="password" type="password" id="password" class="form-control">
+          </div>
+          <div>
+            <button type="submit" class="btn btn-primary ">登入</button>
+            <router-link to="/" class="btn btn-primary mx-2" aria-current="page">返回首頁</router-link>
+          </div>
+        </form>
+        <div class="application">
+          還未有帳號嗎?
+          <router-link to="/application">註冊帳號</router-link>
         </div>
-        <div class="form-group">
-          <label for="password">密碼</label>
-          <input v-model="password" type="password" id="password" class="form-control">
-        </div>
-        <div>
-          <button type="submit" class="btn btn-primary ">登入</button>
-          <router-link to="/" class="btn btn-primary mx-2" aria-current="page">返回首頁</router-link>
-        </div>
-      </form>
-      <div class="application">
-        還未有帳號嗎?
-        <router-link to="/application">註冊帳號</router-link>
-      </div>
-      <p v-if="error" class="error-message">{{ error }}</p>
+        <p v-if="error" class="error-message">{{ error }}</p>
       </div>
     </div>
-    <LayoutHeader/>
-  </template>
+  </div>
+</template>
   
-  <script>
-  export default {
-    data() {
-      return {
-        username: '',
-        password: '',
-        error: '',
-      };
-    },
-    methods: {
-      login() {
-       
-      },
-    },
-  };
-  </script>
-  
-  <style scoped>
-  .login-form {
-    max-width: 500px;
-    margin: 0 auto;
-    padding: 60px;
-    text-align: center;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  }
-  
-  .form-group {
-    margin-bottom: 20px;
-  }
-  
-  .form-control {
-    width: 100%;
-    padding: 10px;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-  }
-  
-  .btn.btn-primary :hover {
-    background-color: #007bff;
-    color: #fff;
-    padding: 10px 20px;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-  }
-  .btn.btn-primary :hover{
-    background-color: #e3e611 ;
-    color: #ac0f8f
-  }
-  
-  .error-message {
-    color: red;
-  }
-  
-  .application{
-    margin-top: 5%;
-  }
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: '',
+      error: '',
+    };
+  },
+  methods: {
+    login() {
 
-  /* 響應視窗*/
-  @media (min-width: 724px) {
-    .login-page{
+    },
+  },
+};
+</script>
+  
+<style scoped>
+.login-form {
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 60px;
+  text-align: center;
+  border: 1px solid #ccc;
+  border-radius: 20px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.form-group {
+  margin-bottom: 20px;
+}
+
+.form-control {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+.btn.btn-primary :hover {
+  background-color: #007bff;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.btn.btn-primary :hover {
+  background-color: #e3e611;
+  color: #ac0f8f
+}
+
+.error-message {
+  color: red;
+}
+
+.application {
+  margin-top: 5%;
+}
+
+/* 響應視窗*/
+/* @media (min-width: 724px) {
+  .login-page {
     min-height: 100vh;
     display: flex;
     align-items: center;
-    }
   }
-  /* .login-form {
+} */
+
+/* .login-form {
     min-height: 100vh;
     display: flex;
     align-items: center;
   } */
-  </style>
+</style>
